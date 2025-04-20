8a52f5a85208287dda22c83470506cb6c7e193aa
kandarpa02 <kandarpaexe@gmail.com>
2025-04-20 18:49:23 +0530

    Update: few things

--- memeGPT/utils/checkpoints.py
+++ memeGPT/utils/checkpoints.py
@@ -4,37 +4,31 @@ import os
 class Checkpoints:
     def __init__(self):
         pass
-    def save_checkpoints(self, model, optimizer, lora_adapter, epoch, loss, path="memeGPT/checkpoints"):
+
+    def save_checkpoints(self, model, optimizer, epoch, loss, path="memeGPT/checkpoints"):
         os.makedirs(path, exist_ok=True)
         rounded_loss = round(loss, 4)
         checkpoint_path = os.path.join(path, f"checkpoint_epoch{epoch}_loss{rounded_loss}.pth")
 
-        checkpoint = {
+        torch.save({
             'epoch': epoch,
+            'loss': loss,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            'loss': loss,
-            'lora_adapter_state_dict': lora_adapter.state_dict()  # Save LoRA adapter state
-        }
+        }, checkpoint_path)
 
-        torch.save(checkpoint, checkpoint_path)
         print(f"Saved checkpoint at: {checkpoint_path}")
 
+    def load_checkpoints(self, model: PeftModel, optimizer, path: str):
 
-    def load_checkpoints(self, model, optimizer, lora_adapter, path):
-        checkpoint = torch.load(path)
-        
-        # Load model weights
-        model.load_state_dict(checkpoint['model_state_dict'], strict=False)
-        
-        # Load optimizer state
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        
-        # Load LoRA adapter state
-        lora_adapter.load_state_dict(checkpoint['lora_adapter_state_dict'])
-        
-        # Retrieve epoch and loss
-        epoch = checkpoint['epoch']
-        loss = checkpoint['loss']
-        
-        return model, optimizer, lora_adapter, epoch, loss
+        ckpt = torch.load(path, map_location=lambda s, l: s.cuda() if torch.cuda.is_available() else s)
+
+        model.load_state_dict(ckpt['model_state_dict'], strict=False)
+        optimizer.load_state_dict(ckpt['optimizer_state_dict'])
+
+        epoch = ckpt['epoch']
+        loss = ckpt['loss']
+        print(f"Loaded checkpoint from epoch {epoch} with loss {loss:.4f}")
+
+        return model, optimizer, epoch, loss
+
