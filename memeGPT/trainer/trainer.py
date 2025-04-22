d80e3918eab6b2f3f1794d2ca2b28ce72c177a46
kandarpa02 <kandarpaexe@gmail.com>
2025-04-22 14:49:15 +0530

    Fix: some major changes

--- memeGPT/trainer/trainer.py
+++ memeGPT/trainer/trainer.py
@@ -1,28 +1,25 @@
 import torch
-from torch.amp import autocast 
-from torch.amp import GradScaler
+from torch.amp import autocast, GradScaler
 
 class Trainer:
     def __init__(self, model, optimizer, mix_precision=False, scaler=None, device='cuda'):
-
         if device == 'cuda' and not torch.cuda.is_available():
             raise RuntimeError("CUDA requested but not available")
-        self.device = device
-
-        self.model = model.to(self.device)
-
+        self.device        = device
+        self.model         = model.to(device)
         self.optimizer     = optimizer
         self.mix_precision = mix_precision
         self.scaler        = scaler or GradScaler()
         self._loss         = 0.0
 
     def process(self, batch):
+        # Move inputs to GPU
         batch = {k: v.to(self.device) for k, v in batch.items()}
 
         self.optimizer.zero_grad()
 
         if self.mix_precision:
-            with autocast(device_type='cuda', dtype=torch.float16):
+            with autocast():                     # defaults to cuda+float16
                 output = self.model(**batch)
                 loss   = output.loss.mean()
 
